beb65e9531106ffbf24fdb727aeb07396d3d615c
Atul Sharma <atulsharma@Atuls-MacBook-Pro.local>
2017-08-26 13:33:37 +0530

    Added code for some more functionalities iOS

--- src/test/java/com/appium/ios/iOS_UICatalog_Start.java
+++ src/test/java/com/appium/ios/iOS_UICatalog_Start.java
@@ -43,11 +43,11 @@ public class iOS_UICatalog_Start {
 
 	}
 
-//	@Test
+	//	@Test
 	public void TestCase_DatePicker() throws InterruptedException {
 		driver.findElement(By.name("Date Picker")).click();
 		Thread.sleep(2000);
-		
+
 		List<IOSElement> wheels = (List<IOSElement>)driver.findElements(By.className("XCUIElementTypePickerWheel"));
 		wheels.get(0).sendKeys("Aug 28");
 		wheels.get(1).setValue("11");
@@ -55,19 +55,19 @@ public class iOS_UICatalog_Start {
 		wheels.get(3).setValue("AM");
 
 		System.out.println(wheels.get(0).getAttribute("value"));
-		
+
 		System.out.println(wheels.get(0).getAttribute("value"));
 		System.out.println(wheels.get(1).getAttribute("value"));
 		System.out.println(wheels.get(2).getAttribute("value"));
 		System.out.println(wheels.get(3).getAttribute("value"));
 
 		Thread.sleep(1000);
-		
+
 		System.out.println(driver.findElements(By.className("XCUIElementTypeStaticText")).get(1).getAttribute("value"));
 		TestCase_001();
 	}
 
-//	@Test
+	//	@Test
 	public void TestCase_PickerView() throws InterruptedException {
 		driver.findElement(By.name("Picker View")).click();
 
@@ -76,36 +76,112 @@ public class iOS_UICatalog_Start {
 		driver.findElementsByAccessibilityId("Green color component value").get(0).sendKeys("50");
 		driver.findElementsByAccessibilityId("Blue color component value").get(0).sendKeys("250");
 		Thread.sleep(1000);
-		
+
 		TestCase_001();
 
 
 	}
-	
+
 	@Test
-	public void TestCase_Switches() {
+	public void TestCase_Switches() throws InterruptedException {
 		driver.findElement(By.name("Switches")).click();
-		
+
 		System.out.println(driver.findElements(By.className("XCUIElementTypeSwitch")).size());
 		String value = (driver.findElements(By.className("XCUIElementTypeSwitch")).get(0).getAttribute("value"));
-		
+
 		if (value=="true") {
 			driver.findElements(By.className("XCUIElementTypeSwitch")).get(0).click();
 			System.out.println(driver.findElements(By.className("XCUIElementTypeSwitch")).get(0).getAttribute("value"));
 			driver.findElements(By.className("XCUIElementTypeSwitch")).get(1).click();
 			System.out.println(driver.findElements(By.className("XCUIElementTypeSwitch")).get(1).getAttribute("value"));
-			
+
 		}
-		 
-			driver.findElements(By.className("XCUIElementTypeSwitch")).get(2).click();
-			System.out.println(driver.findElements(By.className("XCUIElementTypeSwitch")).get(0).getAttribute("value"));
-			driver.findElements(By.className("XCUIElementTypeSwitch")).get(3).click();
-			System.out.println(driver.findElements(By.className("XCUIElementTypeSwitch")).get(1).getAttribute("value"));
-			
+
+		driver.findElements(By.className("XCUIElementTypeSwitch")).get(2).click();
+		System.out.println(driver.findElements(By.className("XCUIElementTypeSwitch")).get(0).getAttribute("value"));
+		driver.findElements(By.className("XCUIElementTypeSwitch")).get(3).click();
+		System.out.println(driver.findElements(By.className("XCUIElementTypeSwitch")).get(1).getAttribute("value"));
+		Thread.sleep(2000);
+		TestCase_001();
+
+
+	}
+	
+	//@Test
+	public void TestCase_Sliders() {
+		driver.findElement(By.name("Sliders")).click();
+		
+		List<IOSElement> sliders = (List<IOSElement>)driver.findElements(By.className("XCUIElementTypeSlider"));
+		
+		System.out.println(sliders.get(0).getAttribute("value"));
+		sliders.get(0).sendKeys("0.6");
+		System.out.println(sliders.get(0).getAttribute("value"));
+	
+		System.out.println(sliders.get(1).getAttribute("value"));
+		sliders.get(0).sendKeys("0.8");
+		System.out.println(sliders.get(1).getAttribute("value"));
+		
+		System.out.println(sliders.get(2).getAttribute("value"));
+		sliders.get(0).sendKeys("0.2");
+		System.out.println(sliders.get(2).getAttribute("value"));
 		
 	}
 	
+	@Test
+	public void TestCase_Steppers() throws InterruptedException {
+		driver.findElement(By.name("Steppers")).click();
+		List<IOSElement> steppers_increment = (List<IOSElement>)driver.findElements(By.name("Increment"));
+		List<IOSElement> steppers_decrement = (List<IOSElement>)driver.findElements(By.name("Decrement"));
+		List<IOSElement> values = (List<IOSElement>)driver.findElements(By.className("XCUIElementTypeStaticText"));
+		
+		System.out.println("value of 1st is : "+values.get(0).getAttribute("value"));
+		System.out.println("value of 2nd is : "+values.get(1).getAttribute("value"));
+		System.out.println("value of 3rd is : "+values.get(2).getAttribute("value"));
+		
+		for(int i=0;i<5;i++)
+		{
+			steppers_increment.get(0).click();
+			steppers_increment.get(1).click();
+			steppers_increment.get(2).click();
+		}
+		System.out.println("value of 1st is : "+values.get(0).getAttribute("value"));
+		System.out.println("value of 2nd is : "+values.get(1).getAttribute("value"));
+		System.out.println("value of 3rd is : "+values.get(2).getAttribute("value"));
+		
+		for(int i=5;i>0;i--)
+		{
+			steppers_decrement.get(0).click();
+			steppers_decrement.get(1).click();
+			steppers_decrement.get(2).click();
+		}
+		System.out.println("value of 1st is : "+values.get(0).getAttribute("value"));
+		System.out.println("value of 2nd is : "+values.get(1).getAttribute("value"));
+		System.out.println("value of 3rd is : "+values.get(2).getAttribute("value"));
+		
+		Thread.sleep(2000);
+		TestCase_001();
+	}
+
+	public void alert_controllers() {
+		
+	}
 	
+	public void stackViews() {
+		
+	}
+	
+	public void Text_Fields() {
+		
+	}
+	
+	public void webView() {
+		
+	}
+	
+	public void research_in_search() {
+		
+	}
+
 
 
 }
